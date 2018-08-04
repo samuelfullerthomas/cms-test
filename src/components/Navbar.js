519967100e1c57ddada18f8b9b4869368e55a091
Samuel Thomas <samuelfullerthomas@gmail.com>
2018-08-04 20:55:20 +0100

    make some things better

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
-import github from '../img/github-icon.svg'
 import logo from '../img/logo.svg'
 
 const Navbar = () => (
@@ -10,7 +9,7 @@ const Navbar = () => (
       <div className="navbar-brand">
         <Link to="/" className="navbar-item">
           <figure className="image">
-            <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
+            <img src={logo} alt="DSA" style={{ width: '88px' }} />
           </figure>
         </Link>
       </div>
@@ -22,18 +21,7 @@ const Navbar = () => (
           Products
         </Link>
       </div>
-      <div className="navbar-end">
-        <a
-          className="navbar-item"
-          href="https://github.com/AustinGreen/gatsby-netlify-cms-boilerplate"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <span className="icon">
-            <img src={github} alt="Github" />
-          </span>
-        </a>
-      </div>
+      <div className="navbar-end" />
     </div>
   </nav>
 )
